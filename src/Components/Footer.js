5d248b2eeda91547ba041b175f0c31dcbb754048
Ålexander Yurchenko <yurchenko@post.com>
2024-11-03 16:13:36 +0100

    Changed Footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,25 +1,36 @@
 export default function Footer() {
   return (
-    <footer className="w-full bg-primary text-light font-rajdhani flex justify-between py-1">
+    <footer className="w-full bg-primary font-rajdhani flex justify-between py-1">
       <ul className="w-1/3 pl-[20px]">
         <li>
-          <a href="Research">Research</a>
+          <a href="Research" className="text-light">
+            Research
+          </a>
         </li>
         <li>
-          <a href="#CV">CV</a>
+          <a href="#CV" className="text-light">
+            CV
+          </a>
         </li>
         <li>
-          <a href="#Teaching">Teaching</a>
+          <a href="#Teaching" className="text-light">
+            Teaching
+          </a>
         </li>
         <li>
-          <a href="#Resources">Resources</a>
+          <a href="#Resources" className="text-light">
+            Resources
+          </a>
         </li>
       </ul>
       <span className="w-1/3 flex flex-col justify-end items-end pr-[20px]">
         <p>
           Created by{" "}
-          <a href="https://yurchenko.vercel.app/" className="hover:underline">
-            Ålexander Yurchenko
+          <a
+            href="https://yurchenko.vercel.app/"
+            className="hover:underline text-light"
+          >
+            Oleksandr Yurchenko
           </a>
         </p>
       </span>
